a0fa5bb3bbc19a0eaa1c93e4afbdb4fd39801cfd
Phil Griswold <philgriswold@me.com>
2019-12-10 12:23:05 -0800

    first

---
+++ DepartmentsEmployees/Models/Data/EmployeeRepository.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using DepartmentsEmployees.Models;
+
+namespace DepartmentsEmployee.Data
+{
+
+    public class EmployeeRepository
+    {
+        /// <summary>
+        ///  Represents a connection to the database.
+        ///   This is a "tunnel" to connect the application to the database.
+        ///   All communication between the application and database passes through this connection.
+        /// </summary>
+        public SqlConnection Connection
+        {
+            get
+            {
+                // This is "address" of the database
+                string _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=DepartmentsEmployees;Integrated Security=True";
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+
+        /// <summary>
+        ///  Returns a list of all departments in the database
+        /// </summary>
+        public List<Employee> GetAllEmployees()
+        {
+            //  We must "use" the database connection.
+            //  Because a database is a shared resource (other applications may be using it too) we must
+            //  be careful about how we interact with it. Specifically, we Open() connections when we need to
+            //  interact with the database and we Close() them when we're finished.
+            //  In C#, a "using" block ensures we correctly disconnect from a resource even if there is an error.
+            //  For database connections, this means the connection will be properly closed.
+            using (SqlConnection conn = Connection)
+            {
+                // Note, we must Open() the connection, the "using" block   doesn't do that for us.
+                conn.Open();
+
+                // We must "use" commands too.
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // Here we setup the command with the SQL we want to execute before we execute it.
+                    cmd.CommandText = "SELECT Id, FirstName, LastName FROM Employee";
+
+                    // Execute the SQL in the database and get a "reader" that will give us access to the data.
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // A list to hold the departments we retrieve from the database.
+                    List<Employee> Employees = new List<Employee>();
+
+                    // Read() will return true if there's more data to read
+                    while (reader.Read())
+                    {
+                        // The "ordinal" is the numeric position of the column in the query results.
+                        //  For our query, "Id" has an ordinal value of 0 and "DeptName" is 1.
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        // We user the reader's GetXXX methods to get the value for a particular ordinal.
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int firstName = reader.GetOrdinal("FirstName");
+                        string firstNameValue = reader.GetString(firstName);
+
+                        int lastName = reader.GetOrdinal("LastName");
+                        string lastNameValue = reader.GetString(lastName);
+
+                        // Now let's create a new department object using the data from the database.
+                        Employee employee = new Employee
+                        {
+                            Id = idValue,
+                            FirstName = firstNameValue,
+                            LastName = lastNameValue,
+                        };
+
+                        // ...and add that department object to our list.
+                        Employees.Add(employee);
+                    }
+
+                    // We should Close() the reader. Unfortunately, a "using" block won't work here.
+                    reader.Close();
+
+                    // Return the list of departments who whomever called this method.
+                    return Employees;
+                }
+            }
+        }
+    
+
+        /// <summary>
+        ///  Returns a single department with the given id.
+        /// </summary>
+        public Employee GetEmployeeById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, LastName FROM Employee WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Employee employee = null;
+
+                    // If we only expect a single row back from the database, we don't need a while loop.
+                    if (reader.Read())
+                    {
+                        employee = new Employee
+                        {
+                            Id = id,
+                            FirstName = reader.GetString(reader.GetOrdinal("First Name")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        };
+                    }
+
+                    reader.Close();
+
+                    return employee;
+                }
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Employee (FirstName, LastNAme, DepartmentId) OUTPUT INSERTED.Id Values (@FirstName, @LastName, @DepartmentId)";
+                    cmd.Parameters.Add(new SqlParameter("@FirstName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@LastName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@DepartmentId", employee.DepartmentId));
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    employee.Id = id;
+                }
+            }
+        }
+
+
+            public void UpdateEmployee(int id, Employee employee)
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Employee
+                                          SET FirstName = @firstName,
+                                          LastName = @lastName,
+                                          DepartmentId = @departmentId
+                                          WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                        cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                        cmd.Parameters.Add(new SqlParameter("@departmentId", employee.DepartmentId));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+
+
+
+            public void DeleteEmployee(int id)
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "DELETE FROM Employee WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+
+      
---
+++ DepartmentsEmployees/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using DepartmentsEmployee.Data;
+using DepartmentsEmployees.Models;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var departmentRepo = new DepartmentRepository();
+            var allDepartments = departmentRepo.GetAllDepartments();
+
+            Console.WriteLine("All Departments");
+            Console.WriteLine("---------------");
+
+            foreach (var dept in allDepartments)
+            {
+                Console.WriteLine(dept.DepartmentName);
+            }
+
+            var hardCodedId = 3;
+            var departmentWithId3 = departmentRepo.GetDepartmentById(hardCodedId);
+
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Department with id {hardCodedId} is {departmentWithId3.DepartmentName}");
+
+            /// heres a break-------------------------------------------------------------------------------
+            /// ---------------------------------------------------------------------------------------------
+
+            var employeeRepo = new EmployeeRepository();
+            var allEmployees = employeeRepo.GetAllEmployees();
+
+            Console.WriteLine("All Employees");
+            Console.WriteLine("---------------");
+
+            foreach (var employee in allEmployees)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+
+            var hardCodedEmployeeId = 3;
+
+            var employeeWithId3 = employeeRepo.GetEmployeeById(hardCodedEmployeeId);
+
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Employee with id {hardCodedEmployeeId} is {employeeWithId3.FirstName}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var legal = new Department();
+            Console.WriteLine("What department would you like to add?");
+            legal.DepartmentName = Console.ReadLine();
+            departmentRepo.AddDepartment(legal);
+
+            Console.WriteLine("What Department (ID) would you like to update?");
+            var departmentToUpdate = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is name of the new department?");
+            var newDepartmentName = Console.ReadLine();
+            departmentRepo.UpdateDepartment(departmentToUpdate, new Department { DepartmentName = newDepartmentName });
+
+            //Add a new employee
+            
+
+            departmentRepo.DeleteDepartment(7);
+            departmentRepo.DeleteDepartment(8);
+            departmentRepo.DeleteDepartment(9);
+        }
+    }
+}
